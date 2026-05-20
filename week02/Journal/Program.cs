b5ea5accb2e58bb4a11d905c4dfd840b543b6da7
Diego Ferreira <dferreiras2002@gmail.com>
2026-05-20 20:52:33 -0300

    Completed Journal Project

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+
+                string response = Console.ReadLine();
+
+                Entry newEntry = new Entry();
+
+                newEntry._date = DateTime.Now.ToShortDateString();
+                newEntry._promptText = prompt;
+                newEntry._entryText = response;
+
+                journal.AddEntry(newEntry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+
+                journal.SaveToFile(filename);
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+
+                journal.LoadFromFile(filename);
+            }
+        }
     }
 }
